708ff38927272ea47a724083d68d03b60b4a718e
Ericson-Manalo <ericmana98@gmail.com>
2023-10-15 20:31:01 +0200

    wrote 'header' to test if i see it on page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,23 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <header>
+        <div>
+            <h1> 
+                header
+            </h1>
+        </div>
+    </header>
 </template>
+
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
+
+<style scoped lang="scss">
+    header{
+        height: 200px;
+        background-color: white;
+        color: black;
+    }
 </style>
