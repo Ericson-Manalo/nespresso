d5d0d020db4a92d082f1bb0d486e35f11fdb0e5b
Ericson-Manalo <ericmana98@gmail.com>
2023-10-16 11:57:20 +0200

    devided upper and lower header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,42 +1,71 @@
 <template lang="">
     <header>
-        <div class="header-container">
+
+            <!--UPPER HEADER-->
+        <div class="upper-header">
             <img class="header-logo" src="https://logos-world.net/wp-content/uploads/2020/12/Nespresso-Logo-700x394.png" alt="Logo Of Nespresso">
             <div class="upper-items">
-                <div>1</div>
-                <div>2</div>
-                <div>3</div>
+                <div>
+                    <a href="">
+                        1
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        2
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        3
+                    </a>
+                </div>
+
             </div>
+        </div>
+
+        <!--LOWER HEADER-->
+        <div class="lower-header">
         </div>
     </header>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            links: [
+
+            ]
+        }
+    },
 }
 </script>
 
 <style scoped lang="scss">
     header{
-        height: 200px;
+        height: 160px;
         background-color: white;
         color: black;
 
-        .header-container{
+        .upper-header{
             display: flex;
+            justify-content: space-between;
+            width: 996px;
+            background-color: red;
+            margin: 0 auto;
 
             img.header-logo{
-                height: 90px
+                height: 80px
             }
 
             div.upper-items{
                 display: flex;
-                height: 90px;
+                height: 80px;
                 align-items: center
-            }
 
-            
+            }
         }
+
     }
 </style>
