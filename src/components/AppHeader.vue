a0e53b5c40f3cd8e755ab2ab1d30f6595d7f035e
Ericson-Manalo <ericmana98@gmail.com>
2023-10-16 15:04:03 +0200

    added drop nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,7 +7,8 @@
             <div class="upper-items">
                 <div>
                     <a href="">
-                        CERCA
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <span>CERCA</span>
                     </a>
                 </div>
                 <div>
@@ -25,14 +26,19 @@
         </div>
 
         <!--LOWER HEADER-->
-        <nav class="lower-header">
-            <ul>
-                <li class="" v-for="link in links">
-                    <router-link :to="{name: link.route}">
-                        {{link.name}}
-                    </router-link>
-                </li>
-            </ul>
+        <nav class="lower-header navbar navbar-expand-lg navbar-light ">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="" v-for="link in links">
+                        <router-link :to="{name: link.route}">
+                            {{link.name}}
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 </template>
