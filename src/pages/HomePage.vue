35246dd520ed4776b2426e4e1b494f44e90e8a53
Ericson-Manalo <ericmana98@gmail.com>
2023-10-17 11:40:16 +0200

    added the jumbotron

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,25 @@
 <template lang="">
-    <div>
-        homepage
-    </div>
+    <section>
+        <div class="col jumbotron">
+
+        </div>
+    </section>
 </template>
+
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
+
+<style lang="scss" scoped>
+
+    div.jumbotron{
+        background-image: url('https://www.nespresso.com/ecom/medias/sys_master/public/28575552045086/autumn-login-hero.jpg');
+        height: 500px;
+        background-position: 50%;
+        background-size: cover;
+        width: 100%;
+    }
+
 </style>
