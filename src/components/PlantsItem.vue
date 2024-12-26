3a7466c58a198e332f8ca3ce3f275f73bca67866
Ericson-Manalo <ericmana98@gmail.com>
2024-12-26 09:57:59 +0100

    created a new PlantsItem components, created cards

---
+++ src/components/PlantsItem.vue
@@ -0,0 +1,45 @@
+<template lang="">
+ <!-- //fare 3 card
+    //1.piante interno
+    //2. piante esterno
+    //3. vasi e arredi -->
+
+    <h2 class="spacing-class">
+        Scopri i nostri prodotti!
+    </h2>
+
+    <div class="row item-card spacing-class">
+        <div class="single-card col-12 col-md-6 col-lg-3">1</div>
+        <div class="single-card col-12 col-md-6 col-lg-3">2</div>
+        <div class="single-card col-12 col-md-6 col-lg-3">3</div>
+        <div class="single-card col-12 col-md-6 col-lg-3">4</div>
+    </div>
+
+</template>
+    
+<script>
+    export default {
+    };
+</script>
+    
+<style scoped lang="scss">
+
+h2{
+    text-transform: uppercase;
+}
+
+div.item-card{
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    gap: 10px;
+
+    div.single-card{
+        height: 350px;
+        border: 1px solid #f2f2f2;
+        width: 250px;
+    }
+}
+
+</style>
+    
